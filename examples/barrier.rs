29cb180861bedb1796a66f55119d95df05bf8df7
Lorenzo <lmartini@student.ethz.ch>
2018-11-27 17:24:56 +0100

    Externalize hist

--- examples/barrier.rs
+++ examples/barrier.rs
@@ -3,6 +3,8 @@ extern crate streaming_harness_hdrhist;
 extern crate core_affinity;
 
 use std::time::Instant;
+use std::rc::Rc;
+use std::cell::RefCell;
 use timely::dataflow::channels::pact::Pipeline;
 use timely::dataflow::operators::{Feedback, ConnectLoop};
 use timely::dataflow::operators::generic::operator::Operator;
@@ -11,17 +13,19 @@ fn main() {
 
     let iterations = std::env::args().nth(1).unwrap().parse::<usize>().unwrap_or(1_000_000);
 
-    timely::execute_from_args(std::env::args().skip(2), move |worker| {
+    let hists = timely::execute_from_args(std::env::args().skip(2), move |worker| {
 
         let index = worker.index();
         // Pin core
         let core_ids = core_affinity::get_core_ids().unwrap();
         core_affinity::set_for_current(core_ids[index % core_ids.len()]);
 
+        let hist1 = Rc::new(RefCell::new(streaming_harness_hdrhist::HDRHist::new()));
+        let hist2 = hist1.clone();
+
         worker.dataflow(move |scope| {
             let (handle, stream) = scope.feedback::<usize>(1);
             let mut t0 = Instant::now();
-            let mut hist = streaming_harness_hdrhist::HDRHist::new();
             stream.unary_notify(
                 Pipeline,
                 "Barrier",
@@ -30,24 +34,26 @@ fn main() {
                     while let Some((cap, _count)) = notificator.next() {
                         let t1 = Instant::now();
                         let duration = t1.duration_since(t0);
-                        hist.add_value(duration.as_secs() * 1_000_000_000u64 + duration.subsec_nanos() as u64);
+                        hist1.borrow_mut().add_value(duration.as_secs() * 1_000_000_000u64 + duration.subsec_nanos() as u64);
                         t0 = t1;
                         let time = *cap.time() + 1;
                         if time < iterations {
                             notificator.notify_at(cap.delayed(&time));
-                        } else {
-                            if index == 0 {
-                                println!("-------------\nSummary:\n{}", hist.summary_string());
-                                println!("-------------\nCDF:");
-                                for entry in hist.ccdf() {
-                                    println!("{:?}", entry);
-                                }
-                            }
                         }
                     }
                 }
-            )
-                .connect_loop(handle);
+            ).connect_loop(handle);
         });
-    }).unwrap(); // asserts error-free execution;
+
+        while worker.step() {}
+
+        let hist = ::std::rc::Rc::try_unwrap(hist2).expect("Non unique owner");
+        hist.into_inner()
+    }).unwrap().join(); // asserts error-free execution;
+
+    println!("-------------\nSummary:\n{}", hists[0].as_ref().unwrap().summary_string());
+    for entry in hists[0].as_ref().unwrap().ccdf() {
+        println!("{:?}", entry);
+    }
+
 }

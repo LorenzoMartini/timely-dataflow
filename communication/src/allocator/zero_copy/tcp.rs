580eac6e6bbf4ad1d9018e88c23e2ce3082c8189
Lorenzo <lmartini@student.ethz.ch>
2019-02-19 10:28:45 +0100

    Remove nonblocking

--- communication/src/allocator/zero_copy/tcp.rs
+++ communication/src/allocator/zero_copy/tcp.rs
@@ -6,7 +6,6 @@ use std::net::TcpStream;
 use self::amd64_timer::ticks;
 use networking::MessageHeader;
 
-use std::io::ErrorKind::WouldBlock;
 use super::bytes_slab::BytesSlab;
 use super::bytes_exchange::{MergeQueue, Signal};
 
@@ -49,33 +48,21 @@ pub fn recv_loop(
     let mut hist = streaming_harness_hdrhist::HDRHist::new();
     let mut hist_n_bytes = streaming_harness_hdrhist::HDRHist::new();
 
-    let mut counterz: usize = 0;
     while active {
         buffer.ensure_capacity(1);
 
         assert!(!buffer.empty().is_empty());
 
         // Attempt to read some more bytes into self.buffer.
-        let mut read = 0;
-        let mut t0 = ticks();
-        reader.set_nonblocking(true).expect("OPS");
-        while read <= 0 {
-            read = match reader.read(&mut buffer.empty()) {
-                Ok(n) => n,
-                Err(x) => match x.kind() {
-                    WouldBlock => {
-                        counterz+=1;
-                        t0 = ticks();
-                        0
-                    },
-                    // We don't expect this, as socket closure results in Ok(0) reads.
-                    _ => {
-                        println!("Error: {:?}", x);
-                        0
-                    }
-                },
-            };
-        }
+        let t0 = ticks();
+        let read = match reader.read(&mut buffer.empty()) {
+            Ok(n) => n,
+            Err(x) => {
+                // We don't expect this, as socket closure results in Ok(0) reads.
+                println!("Error: {:?}", x);
+                0
+            },
+        };
         let t1 = ticks();
         hist.add_value(t1 - t0);
         hist_n_bytes.add_value(read as u64);
@@ -104,20 +91,8 @@ pub fn recv_loop(
                     panic!("Clean shutdown followed by data.");
                 }
                 buffer.ensure_capacity(1);
-
-                // Shutdown
-                let mut read = -1;
-                while read <= -1 {
-                    read = match reader.read(&mut buffer.empty()) {
-                        Ok(n) => n as isize,
-                        Err(err) => match err.kind() {
-                            WouldBlock => -1,
-                            _ => panic!("Error occurred while shutting down stream")
-                        }
-                    }
-                }
-                if read > 0 {
-                    panic!("Clean shutdown followed by data");
+                if reader.read(&mut buffer.empty()).expect("read failure") > 0 {
+                    panic!("Clean shutdown followed by data.");
                 }
             }
         }
@@ -133,12 +108,12 @@ pub fn recv_loop(
     // Log the receive thread's start.
     logger.as_mut().map(|l| l.log(StateEvent { send: false, process, remote, start: false, }));
 
-    println!("------------\nRead summary\n---------------{}", counterz);
+    println!("------------\nRead summary\n---------------");
     println!("{}", hist.summary_string());
     for entry in hist.ccdf() {
         println!("{:?}", entry);
     }
-    println!("------------\nbytes summary\n---------------{}", counterz);
+    println!("------------\nbytes summary\n---------------");
     println!("{}", hist_n_bytes.summary_string());
     for entry in hist_n_bytes.ccdf() {
         println!("{:?}", entry);
@@ -180,45 +155,28 @@ pub fn send_loop(
             // still be a signal incoming.
             //
             // We could get awoken by more data, a channel closing, or spuriously perhaps.
-            let mut flushed = false;
-            while !flushed {
-                match writer.flush() {
-                    Ok(_) => flushed = true,
-                    Err(err) => match err.kind() {
-                        WouldBlock => (),
-                        _ => panic!("Fail flush")
-                    }
-                }
-            }
+            writer.flush().expect("Failed to flush writer.");
             sources.retain(|source| !source.is_complete());
             if !sources.is_empty() {
                 signal.wait();
             }
         }
-        else {
-            // TODO: Could do scatter/gather write here.
-            for mut bytes in stash.drain(..) {
+            else {
+                // TODO: Could do scatter/gather write here.
+                for mut bytes in stash.drain(..) {
 
-                // Record message sends.
-                logger.as_mut().map(|logger| {
-                    let mut offset = 0;
-                    while let Some(header) = MessageHeader::try_read(&mut bytes[offset..]) {
-                        logger.log(MessageEvent { is_send: true, header, });
-                        offset += header.required_bytes();
-                    }
-                });
-                let mut written = false;
-                while !written {
-                    match writer.write_all(&bytes[..]) {
-                        Ok(_) => written = true,
-                        Err(err) => match err.kind() {
-                            WouldBlock => (),
-                            _ => panic!("Write failure in send_loop.")
+                    // Record message sends.
+                    logger.as_mut().map(|logger| {
+                        let mut offset = 0;
+                        while let Some(header) = MessageHeader::try_read(&mut bytes[offset..]) {
+                            logger.log(MessageEvent { is_send: true, header, });
+                            offset += header.required_bytes();
                         }
-                    }
+                    });
+
+                    writer.write_all(&bytes[..]).expect("Write failure in send_loop.");
                 }
             }
-        }
     }
 
     // Write final zero-length header.
@@ -232,16 +190,7 @@ pub fn send_loop(
         seqno:      0,
     };
     header.write_to(&mut writer).expect("Failed to write header!");
-    let mut flushed = false;
-    while !flushed {
-        match writer.flush() {
-            Ok(_) => flushed = true,
-            Err(err) => match err.kind() {
-                WouldBlock => (),
-                _ => panic!("Fail flush")
-            }
-        }
-    }
+    writer.flush().expect("Failed to flush writer.");
     writer.get_mut().shutdown(::std::net::Shutdown::Write).expect("Write shutdown failed");
     logger.as_mut().map(|logger| logger.log(MessageEvent { is_send: true, header }));
 

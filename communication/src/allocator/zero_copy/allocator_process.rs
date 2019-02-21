b9f80776a9dd8c1ab32a52459072f82a826e48e7
Lorenzo <lmartini@student.ethz.ch>
2019-02-21 10:35:00 +0100

    add print to allocator proc'

--- communication/src/allocator/zero_copy/allocator_process.rs
+++ communication/src/allocator/zero_copy/allocator_process.rs
@@ -156,6 +156,9 @@ impl Allocate for ProcessAllocator {
     // Perform preparatory work, most likely reading binary buffers from self.recv.
     #[inline(never)]
     fn receive(&mut self) {
+        if self.index == 0 {
+            println!("W{}", self.index);
+        }
 
         // Check for channels whose `Puller` has been dropped.
         let mut canaries = self.canaries.borrow_mut();

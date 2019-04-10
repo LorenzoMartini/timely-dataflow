22195c5454bcca188bf827f0dfaeb0bfe99466fe
Lorenzo <lmartini@student.ethz.ch>
2019-04-10 14:13:43 +0200

    Measure write delay

--- communication/src/allocator/zero_copy/initialize.rs
+++ communication/src/allocator/zero_copy/initialize.rs
@@ -61,7 +61,6 @@ pub fn initialize_networking(
     for index in 0..results.len() {
 
         if let Some(stream) = results[index].take() {
-            // remote process
 
             let (remote_recv, signal) = remote_recv_iter.next().unwrap();
 
